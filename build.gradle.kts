66dc1a3b765f69c5124418ad9ca6d4718118d834
SuL <jjw011202@naver.com>
2020-09-29 01:01:46 +0900

    Kotlin STD Lib을 외부로 분리

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    compileOnly(files("C:/Users/PHR/Desktop/PluginStorage/KotlinLoader_S-1.4.10.jar"))
     compileOnly("com.destroystokyo.paper", "paper-api", "1.12.2-R0.1-SNAPSHOT")
 }
 
@@ -26,22 +26,13 @@ spigot {
 
 tasks.compileJava.get().options.encoding = "UTF-8"
 
-// type = Jar::class가 상속 비슷한 느낌인건가?
-val fatJar = task("fatJar", type = Jar::class) {
-    archiveFileName.set("${project.name}_S.jar")
-    destinationDirectory.set(file("C:/Users/PHR/Desktop/PluginStorage"))
-    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
-    with(tasks.jar.get() as CopySpec)
-}
-
 tasks {
     compileKotlin.get().kotlinOptions.jvmTarget = "1.8"
     compileTestKotlin.get().kotlinOptions.jvmTarget = "1.8"
 
     jar {
-        dependsOn(fatJar)
-        archiveFileName.set("${project.name}_S-NotFat.jar")
-        destinationDirectory.set(file("C:/Users/PHR/Desktop/PluginStorage/NotFat"))
+        archiveFileName.set("${project.name}_S.jar")
+        destinationDirectory.set(file("C:/Users/PHR/Desktop/PluginStorage"))
     }
 }
 
